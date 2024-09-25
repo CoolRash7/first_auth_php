b0122a9958bded1cbd79877af1c11218a5e93248
CoolRash7 <47377195+CoolRash7@users.noreply.github.com>
2024-09-25 22:05:54 +0900

    Add files via upload

--- user/upload_avatar.php
+++ user/upload_avatar.php
@@ -39,5 +39,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else {
         echo "невозможно загрузить файл!";
     }
+
+        // Проверка допустимости файла
+        if ($file && isset($file['error']) && $file['error'] == UPLOAD_ERR_OK) {
+            // Проверка расширения файла
+            if (preg_match('/^image\/(jpeg|jpg|png)$/i', $type)) {
+                if ($size <= 1048576) {
+                    // Сохранение файла
+                    $target_path = $dir.'/'.uniqid('img_', true).'_'.$filename;
+                    if (move_uploaded_file($tmp_name , $target_path)) {
+
+                        $session_name = $_SESSION['name'];
+                        $mysql->query("UPDATE `users` SET `img_avatar`='$target_path' WHERE `name`='$session_name' ");
+
+                        echo "Файл успешно сохранен в {$target_path}";
+                    } else {
+                        echo "Ошибка при сохранении файла!";
+                    }
+                } else {
+                    echo "Изображение должно быть меньше 1 МБ";
+                }
+    
+            } else {
+                echo "Только JPEG, JPG или PNG файлы допускаются.";
+            }
+        } else {
+            echo "невозможно загрузить файл!";
+        }
 }
 ?>
