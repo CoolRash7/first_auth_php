b2d1286b557dd319fcf0e47459039b221d6759c5
CoolRash7 <47377195+CoolRash7@users.noreply.github.com>
2024-09-25 22:05:27 +0900

    Add files via upload

--- upload_image.php
+++ upload_image.php
@@ -12,18 +12,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $filename = $file['name'];
     $tmp_name = $file['tmp_name'];
     $type = $file['type'];
+    $size = $file['size'];
 
     // Проверка допустимости файла
     if ($file && isset($file['error']) && $file['error'] == UPLOAD_ERR_OK) {
         // Проверка расширения файла
         if (preg_match('/^image\/(jpeg|jpg|png)$/i', $type)) {
-            // Сохранение файла
-            $target_path = $dir.'/'.basename($filename);
-            if (move_uploaded_file($tmp_name , $target_path)) {
-                echo "Файл успешно сохранен в {$target_path}";
+            if ($size <= 1048576) {
+                // Сохранение файла
+                $target_path = $dir.'/'.uniqid('img_', true).'_'.$filename;
+                if (move_uploaded_file($tmp_name , $target_path)) {
+                    echo "Файл успешно сохранен в {$target_path}";
+                } else {
+                    echo "Ошибка при сохранении файла!";
+                }
             } else {
-                echo "Ошибка при сохранении файла!";
+                echo "Изображение должно быть меньше 1 МБ";
             }
+
         } else {
             echo "Только JPEG, JPG или PNG файлы допускаются.";
         }
